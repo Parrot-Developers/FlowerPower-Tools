ce2217793f52c553772372ef7af88b5205cdd891
bsautron <sautron.brunojj@gmail.com>
2015-12-01 16:09:33 +0100

    Clean version

--- BLE-cloud-bridge/index.js
+++ BLE-cloud-bridge/index.js
@@ -7,6 +7,12 @@ var Parrot = new CloudAPI(credentials.url);
 var myPannel = new Pannel(Parrot);
 delete credentials.url;
 
+
+var options = {
+	delay: 1,
+	type: ['pot'],
+	priority: ['Flower power 0CB8']
+}
 myPannel.loginToApi(credentials, function(err, res){
-  myPannel.automatic();
+  myPannel.automatic(options);
 });
